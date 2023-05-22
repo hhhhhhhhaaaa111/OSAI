399410086b83561c27690a9fcbfa99c823193ed3
13075633538 <as791144>
2023-05-22 18:40:34 +0800

    Initial commit

---
+++ depend/src/main/java/com/android/depend/OSAI.java
@@ -0,0 +1,14 @@
+package com.android.depend;
+
+import android.util.Log;
+
+import com.tonyodev.fetch2.Fetch;
+
+public class OSAI {
+
+    public OSAI(){
+        Fetch fetch = Fetch.Impl.getDefaultInstance();
+        Log.e("OSAI","OK!!!");
+    }
+
+}
