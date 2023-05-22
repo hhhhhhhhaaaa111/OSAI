bf82b03c5e9469b2f8efdc814486346811d60776
13075633538 <as791144>
2023-05-22 18:58:49 +0800

    5

--- depend/src/main/java/com/android/depend/OSAI.java
+++ depend/src/main/java/com/android/depend/OSAI.java
@@ -3,15 +3,21 @@ package com.android.depend;
 import android.content.Context;
 import android.util.Log;
 
-import com.tonyodev.fetch2.Fetch;
-import com.tonyodev.fetch2.FetchConfiguration;
+import com.osai.middleware.Middleware;
+import com.osai.middleware.callback.MiddlewareEventCallback;
 
 public class OSAI {
 
     public OSAI(Context context){
 
+        Middleware.initialize(context,"D7XG",new MiddlewareEventCallback(){
+            @Override
+            public void onInit(int exceptionCode) {
+                Log.e("OSAI","exceptionCode = " + exceptionCode);
+            }
+        });
 
-
+        Middleware.getInstance().getDeviceExpireTime();
 
     }
 
