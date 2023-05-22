2617693f049b374615f54aed98b93d954cc92cfc
13075633538 <as791144>
2023-05-22 18:50:26 +0800

    2

--- depend/src/main/java/com/android/depend/OSAI.java
+++ depend/src/main/java/com/android/depend/OSAI.java
@@ -1,13 +1,16 @@
 package com.android.depend;
 
+import android.content.Context;
 import android.util.Log;
 
 import com.tonyodev.fetch2.Fetch;
+import com.tonyodev.fetch2.FetchConfiguration;
 
 public class OSAI {
 
-    public OSAI(){
-        Fetch fetch = Fetch.Impl.getDefaultInstance();
+    public OSAI(Context context){
+        FetchConfiguration fetchConfiguration = new FetchConfiguration.Builder(context).build();
+        Fetch fetch = Fetch.Impl.getInstance(fetchConfiguration);
         Log.e("OSAI","OK!!!");
     }
 
